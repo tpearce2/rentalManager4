e3685757f95d45d204f96214da71baa2b7b94633
Tyler Pearce <tyler@teemedia.ca>
2012-07-09 22:18:32 -0400

    m

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -31,10 +31,10 @@ class CustomerController < ApplicationController
       
       customer = Customer.where('email = ?', params[:email]).first 
       subscription = Subscription.where('subscriptionID = ?', params[:sID]).first 
-      if(!customer.blank? && !subscription.blank
+      if(!customer.blank? && !subscription.blank)
           emailStatus = customer.sendCancelEmail(subscription)
           render :json => { :msg => emailStatus}, :callback => params[:callback]
-      else 
+      else
         render :json => { :error => 'Invalid Email'}, :callback => params[:callback] 
       end
       
